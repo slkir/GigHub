406c7e7ab8f7dd36af8f28434230091aa64910e1
Viacheslav <maker1992@mail.ru>
2020-10-11 13:30:43 +0300

    Create the gig model.

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]  
+        [StringLength(256)]
+        public string Name { get; set; }
+    }
+}
