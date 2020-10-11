0b22914ec029b9359d75c7d205b77908fd6c9bb3
Viacheslav <maker1992@mail.ru>
2020-10-11 19:51:50 +0300

    Move DateTime calculation to GigFormViewModel.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,23 @@
 ﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(Date + " " + Time);
+            }
+        }
+
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
