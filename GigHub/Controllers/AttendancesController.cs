fb5d3b1c682e738a8b9271357e9c65e7651aae3f
Viacheslav <maker1992@mail.ru>
2020-10-13 14:45:50 +0300

    Added going button.

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+                return BadRequest("Attendance already exists.");
+
+            var attendace = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendace);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
